7b3ec149b69c1e3d47a1b6c3dbd8bfbbdd9e0e51
Eskild Skov Pedersen <eskild.skov.pedersen@gmail.com>
2020-03-15 16:16:36 +0100

    added multiple base layers and overlays

--- script.js
+++ script.js
@@ -1,29 +1,89 @@
-var map = L.map('map').setView([62.6112822, 7.907181], 8);
+// let map = L.map('map').setView([62.6112822, 7.907181], 8);
 
-L.tileLayer(
-  'https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=toporaster3&zoom={z}&x={x}&y={y}',
+// if (!map.restoreView()) {
+//   map.setView([62.2587306, 6.3106444], 14);
+// }
+
+let attribution = '<a href="http://www.kartverket.no/">Kartverket</a>';
+
+let rasterBaseMap = L.tileLayer(
+  'https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=toporaster3&zoom={z}&x={x}&y={y}'
+);
+
+let vectorBaseMap = L.tileLayer(
+  'http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}'
+);
+
+let kastOverlayMap = L.esri.dynamicMapLayer({
+  url:
+    'https://gis3.nve.no/map/rest/services/Mapservices/KlassifiseringSkredterreng/MapServer',
+  layers: [4, 5, 6, 7],
+});
+
+let steepnessOverlayMap = L.tileLayer.wms(
+  'https://gis3.nve.no/map/services/Bratthet/MapServer/WmsServer?',
   {
-    attribution: '<a href="http://www.kartverket.no/">Kartverket</a>',
+    layers: 'Bratthet_snoskred',
+    format: 'image/png',
+    transparent: 'true',
   }
-).addTo(map);
+);
+
+let baseMaps = {
+  Vektorkart: vectorBaseMap,
+  Rasterkart: rasterBaseMap,
+};
+
+let overlayMaps = {
+  Bratthetskart: steepnessOverlayMap,
+  AutoKAST: kastOverlayMap,
+};
+
+// let map = L.map('map').setView([62.6112822, 7.907181], 8);
+
+let map = L.map('map', {
+  center: [62.6112822, 7.907181],
+  zoom: 8,
+  layers: vectorBaseMap,
+});
+
+let controlLayersOptions = {
+  position: 'topleft',
+  collapsed: false,
+};
+
+// L.control.layers(baseMaps, '', controlLayersOptions).addTo(map);
+
+let groupedOverlays = {
+  Tillegg: {
+    Bratthet: steepnessOverlayMap,
+    AutoKAST: kastOverlayMap,
+  },
+};
+
+let groupLayersOptions = {
+  // Make the "Landmarks" group exclusive (use radio inputs)
+  exclusiveGroups: ['Tillegg'],
+  // Show a checkbox next to non-exclusive group labels for toggling all
+  // groupCheckboxes: true,
+  position: 'topleft',
+  collapsed: false,
+};
+
+L.control
+  .groupedLayers(baseMaps, groupedOverlays, groupLayersOptions)
+  .addTo(map);
 
 if (!map.restoreView()) {
   map.setView([62.2587306, 6.3106444], 14);
 }
 
-var wmsLayer = L.esri
-  .dynamicMapLayer({
-    url:
-      'https://gis3.nve.no/map/rest/services/Mapservices/KlassifiseringSkredterreng/MapServer',
-    layers: [4, 5, 6, 7],
-    opacity: 0.5,
-  })
-  .addTo(map);
+let controlLayer = kastOverlayMap;
 
 var slider = L.control
   .slider(
     function(value) {
-      wmsLayer.setOpacity(value);
+      controlLayer.setOpacity(value);
     },
     {
       min: 0,
@@ -31,7 +91,6 @@ var slider = L.control
       step: 0.01,
       position: 'topleft',
       collapsed: false,
-      getValue: wmsLayer.opacity,
       syncSlider: true,
       title: 'Gjennomsiktighet',
       showValue: false,
@@ -54,3 +113,18 @@ L.control
     lineWeight: 3,
   })
   .addTo(map);
+
+L.control.scale({ imperial: false, maxWidth: 200 }).addTo(map);
+
+function changeOverlayControl(e) {
+  controlLayer = e.layer;
+  if (slider.slider.value == 0) {
+    let val = 0.2;
+    controlLayer.setOpacity(val);
+    slider.slider.value = val;
+  } else {
+    controlLayer.setOpacity(slider.slider.value);
+  }
+}
+
+map.on('overlayadd', changeOverlayControl);
