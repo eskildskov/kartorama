de7128f80317a7a5232355963987338d2a9a7d67
Eskild Skov Pedersen <eskild@eskildskov.no>
2021-03-14 20:22:22 +0100

    added more info in sidebar + styling

--- src/routes/src/RouteListItem.vue
+++ src/routes/src/RouteListItem.vue
@@ -7,19 +7,59 @@
     <div class="media-content">
       <div class="content">
         <p>
-          <strong>{{ route.mountain_name }}</strong>
+          <strong class="mountain-name">{{ route.mountain_name }}</strong>
           <small v-if="route.route_name != 'Normalveien'">{{ route.route_name }}</small>
-          <i class="iconify ml-2" data-icon="mdi-car"></i> <small>{{ route.max_start_altitude }} moh</small>
-          <i class="iconify ml-2" data-icon="mdi:compass-outline"></i> <small>{{ route.aspect.join(', ') }}</small>
-          <i class="iconify ml-2" data-icon="bi:globe"></i> <small>{{ route.area }}</small>
-          <span v-if="isActive"><br/>
-            <span v-if="route.avalanche_start_zones.length">
-              <i class="iconify" data-icon="mdi:angle-acute"></i>
-              <span v-for="z in route.avalanche_start_zones">
-                {{ z[0] }}: {{z[1]}}°.
-              </span>
 
+          <span v-show="isActive === false">
+            <i class="iconify ml-2" data-icon="mdi-car"></i> <small>{{ route.max_start_altitude }} moh</small>
+            <i class="iconify ml-2" data-icon="mdi:compass-outline"></i> <small>{{ route.aspect.join(', ') }}</small>
+          </span>
+
+          <span v-show="isActive">
+            <br />
+            <i class="iconify mr-1" data-icon="fa-solid:traffic-light"></i>
+            <strong>KAST: </strong>{{ route.kast }}. <strong>Eksponering: </strong>{{ route.exposure }}. <strong>Vanskelighetsgrad: </strong>{{ route.difficulty }}.
+
+
+            <br />
+            <i class="iconify mr-1" data-icon="mdi-car"></i><strong>Parkering:</strong>
+            <template v-if="route.start_location">{{route.start_location}}, </template>
+
+{{ route.max_start_altitude }} moh
+            <br />
+            <i class="iconify mr-1" data-icon="mdi:compass-outline"></i><strong>Retning:</strong> {{ route.aspect.join(', ') }}
+            <br />
+            <i class="iconify mr-1" data-icon="bi:globe"></i><strong>Område:</strong>
+<span>{{ route.area }}</span>
+
+            <span v-if="route.avalanche_start_zones.length">
+              <br />
+              <i class="iconify" data-icon="mdi:angle-acute"></i>
+              <strong>Løsneområder: </strong>
+              <span class="mr-1" v-for="z in route.avalanche_start_zones">
+                {{ z[0] }} moh: {{z[1]}}°.
+              </span>
             </span>
+
+            <span v-if="route.dangers">
+              <br />
+              <i class="iconify" data-icon="jam:triangle-danger-f"></i>
+              <strong>Farer: </strong><span>{{ route.dangers }}</span>
+            </span>
+
+            <span v-if="route.equipment">
+              <br />
+              <i class="iconify" data-icon="whh:pickaxe"></i>
+              <strong>Utstyr: </strong><span>{{ route.equipment }}</span>
+            </span>
+
+
+            <span v-if="route.comment">
+              <br />
+              <i class="iconify" data-icon="fa-solid:lightbulb"></i>
+              <strong>Kommentar: </strong><span>{{ route.comment }}</span>
+            </span>
+
           </span>
         </p>
 
@@ -69,8 +109,13 @@ article:hover {
   cursor: pointer;
 }
 
-.active {
+article.active {
   background:rgb(240, 240, 240);
+  cursor:auto;
   font-size: 1.1em;
+}
+
+.active .mountain-name {
+  font-size:1.2em
 }
 </style>
