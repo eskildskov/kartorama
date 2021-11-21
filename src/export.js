e81da2e72ebea6abd36442c49ade7811107709c4
eskildskov <eskild.skov.pedersen@gmail.com>
2021-11-21 09:27:17 +0100

    change endpoint to export.js

---
+++ src/export.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unused-modules */
+export {
+  initScaleControl,
+  initZoomControl,
+  initOpacityControl,
+  initLayerControl,
+  initLocateControl,
+  initFileLoaderControl,
+  initMap,
+} from "./controls.js";
+
+export { RouteHandler, allUserRoutes } from "./routes.js";
+
+export { initSaveRouteControl } from "./save-route.js";
