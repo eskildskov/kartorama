592ec0fd8e512bad345476021b0eb13cdf0fb46c
eskildskov <eskild.skov.pedersen@gmail.com>
2022-01-10 20:45:18 +0100

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,31 @@
 /* eslint-disable */
 
 import { defineConfig } from "vite";
+import { dependencies } from "./package.json";
+
+const vendors = [
+  "leaflet",
+  "@turf/turf",
+  "@turf/meta",
+  "@turf/helpers",
+  "@geoman-io/leaflet-geoman-free",
+  "@fortawesome/fontawesome-free",
+];
 
 export default defineConfig({
   root: "src",
+
   build: {
     outDir: "../dist",
-    //   lib: {
-    //     entry: "export.js",
-    //     name: "Kartorama",
-    //     fileName: (format) => `kartorama.${format}.js`,
-    //     formats: ["es"],
-    //   },
-    //   rollupOptions: {
-    //     // make sure to externalize deps that shouldn't be bundled
-    //     // into your library
-    //     // external: ["vue"],
-    //     output: {
-    //       // Provide global variables to use in the UMD build
-    //       // for externalized deps
-    //       globals: {
-    //         // vue: "Vue",
-    //       },
-    //     },
-    //   },
+    emptyOutDir: true,
+    sourceMap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: vendors,
+          // ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
