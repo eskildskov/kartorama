d66f3eb314d3d969fb5209a0a86eb086cce80bff
eskildskov <eskild.skov.pedersen@gmail.com>
2022-01-10 21:42:26 +0100

    rm dist

--- dist/assets/deflate.a9d67654.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./pako.esm.51c05ca9.js";import{B as a}from"./basedecoder.7014bb2c.js";class t extends a{decodeBlock(e){return r(new Uint8Array(e)).buffer}}export{t as default};
--- dist/assets/raw.687e8cce.js
+++
@@ -1,1 +0,0 @@
-import{B as d}from"./basedecoder.7014bb2c.js";class s extends d{decodeBlock(e){return e}}export{s as default};
